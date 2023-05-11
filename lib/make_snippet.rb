aededc8f7694a01bdb21672dd655ec094b06085e
Della Izadi <dellaizadi@MacBook-Air.local>
2023-05-11 15:37:12 +0100

    Adding 5 words and less - first 2 tests

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,7 +1,3 @@
 def make_snippet(str)
-  #words = str.split(‘ ‘)
-
-  if str.length <= 5
-    return str
-  end
+  return str
 end
--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -2,7 +2,12 @@ require 'make_snippet'
 
 RSpec.describe "make snippet method" do
   it "if less than 5 words it returns first 5 words" do
-    result = make_snippet("cat")
-    expect(result).to eq "cat"
+    result = make_snippet("Hello cat I'm here")
+    expect(result).to eq "Hello cat I'm here"
+  end 
+
+  it "if it is 5 words" do
+    result = make_snippet("Hello cat I'm here now")
+    expect(result).to eq "Hello cat I'm here now"
   end
 end
