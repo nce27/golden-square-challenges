3e40d66e54a6e1060a49e6a15b902f2986a7c3ec
nce27 <nc3elliott@gmail.com>
2023-05-11 16:59:33 +0100

    Updated program to output the appropriate message with a  if there are more than 5 words

--- lib/make_snippet.rb
+++ lib/make_snippet.rb
@@ -1,3 +1,11 @@
 def make_snippet(str)
-  return str
+  words = str.split(' ')
+  #words.size > 5 ? "#{words.first(5).join(' ')}..." : words.join(' ')
+  if words.length <= 5
+    return str
+  else
+    first_five_words = words[0, 5].join(" ")
+    return first_five_words + "..."
+  end
+
 end
--- spec/make_snippet_spec.rb
+++ spec/make_snippet_spec.rb
@@ -1,5 +1,6 @@
 require 'make_snippet'
 
+require 'make_snippet'
 RSpec.describe "make snippet method" do
   it "if less than 5 words it returns first 5 words" do
     result = make_snippet("Hello cat I'm here")
@@ -10,4 +11,10 @@ RSpec.describe "make snippet method" do
     result = make_snippet("Hello cat I'm here now")
     expect(result).to eq "Hello cat I'm here now"
   end
+
+  it "if it is more than 5 words" do
+    result = make_snippet("Hello cat I'm here now again")
+    expect(result).to eq "Hello cat I'm here now..."
+  end
+  
 end
