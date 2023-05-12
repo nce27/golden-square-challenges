481de25095ac1b964f9e8c1a7de909fdf48e66e0
Della Izadi <dellaizadi@MacBook-Air.local>
2023-05-12 10:57:20 +0100

    Adding the second block of test that passed

--- lib/count_words.rb
+++ lib/count_words.rb
@@ -1,3 +1,5 @@
 def count_words(str)
-    return str
+    #return str
+    words = str.split(" ")
+    return words.count
 end
--- spec/count_words_spec.rb
+++ spec/count_words_spec.rb
@@ -3,6 +3,11 @@ require "count_words"
 RSpec.describe "it takes a string as an argument" do
     it "it takes the string" do
         result = count_words("Hello world")
-        expect(result).to eq "Hello world"
+        expect(result).to eq 2
+    end
+
+    it "returns the number of words" do
+        result = count_words("Hello world Della")
+        expect(result).to eq 3
     end
 end 
